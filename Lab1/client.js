3c3cac6265af95bf78e2efcc575a8a3cc5cb6aa1
Fredrik Nilgert <fredrik03@live.se>
2026-01-27 02:31:24 +0100

    implemented views

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,11 +1,22 @@
-displayView = function(){
+displayView = function(selectedView){
     // the code required to display a view
+    const selection = document.getElementById(selectedView);
+    const view = document.getElementById("view");
 
+    view.innerHTML = selection.innerHTML;
 };
 
 window.onload = function(){
     //code that is executed as the page is loaded.
     //You shall put your own custom code here.
     //window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+    //window.alert("Hello TDDD97!");
+
+    const loggedIn = false;
+
+    if (loggedIn) {
+        displayView("profileView")
+    } else {
+        displayView("welcomeView")
+    };
 };
