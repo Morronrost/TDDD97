8bca77cae21274985737b112b75661b6ef743e43
Fredrik Nilgert <fredrik03@live.se>
2026-02-17 15:23:33 +0100

    worked on lab2

---
+++ Lab2/database_helper.py
@@ -0,0 +1,77 @@
+import sqlite3
+from flask import g
+import random
+
+DATABASE_URI="database.db"
+
+def init_db():
+    db = get_db()
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT NOT NULL,
+        firstname TEXT NOT NULL,
+        familyname TEXT NOT NULL,
+        gender TEXT NOT NULL,
+        city TEXT NOT NULL,
+        country TEXT NOT NULL,
+        token TEXT
+    )
+    """)
+    db.commit()
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = sqlite3.connect(DATABASE_URI)
+        db.row_factory = sqlite3.Row
+    return db
+
+def get_user_by_email(email):
+    db = get_db()
+    cursor = db.cursor()
+
+    user = cursor.execute("SELECT email, password FROM users WHERE email=?", (email,)).fetchone()
+
+    return user
+
+
+
+
+def sign_up(email, password, firstname, familyname, gender, city, country):
+    db = get_db()
+    cursor = db.cursor()
+    
+    try:
+        cursor.execute("INSERT INTO users (email, password, firstname, familyname, gender, city, country) VALUES (?, ?, ?, ?, ?, ?, ?)", (email, password, firstname, familyname, gender, city, country))
+        db.commit()
+        db.close()
+        return True
+    except sqlite3.IntegrityError:
+        db.close()
+        return False
+
+def sign_in(email, password):
+    db = get_db()
+    cursor = db.cursor()
+
+    user = cursor.execute("SELECT email, password FROM users WHERE email=?", (email,)).fetchone()
+
+    if not user:
+        return False, None
+
+    if not (user["password"] == password):
+        return False, None
+
+    letters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    token = ""
+
+    for i in range(0,36):
+        token += random.choice(letters)
+    
+    cursor.execute("UPDATE users SET token=? WHERE email=?", (token, email,))
+    db.commit()
+    db.close()
+    
+    return True, token
+
--- Lab2/server.py
+++ Lab2/server.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+import database_helper
 
 app = Flask(__name__)
 
-@app.route("/sign_in", methods=["GET", "POST"])
+with app.app_context():
+    database_helper.init_db()
+
+@app.route("/sign_up", methods=["POST"])
+def sign_up():
+    data = request.get_json()
+    required = ["email", "password", "firstname", "familyname", "gender", "city", "country"]
+    
+    if not all(data.get(field) for field in required):
+        return jsonify(success=False, message="Invalid data field.", data=None), 400
+
+    success = database_helper.sign_up(data.get("email"), data.get("password"), data.get("firstname"), data.get("familyname"), data.get("gender"), data.get("city"), data.get("country"))
+
+    if not success:
+        return jsonify(success=False, message="User already exists.", data=None), 400
+
+    return jsonify(success=True, message="User created.", data=None), 201
+
+
+
+@app.route("/sign_in", methods=["POST"])
 def sign_in():
-    return "<p>Hello, World!</p>"
+    data = request.get_json()
+
+    if not data:
+        return jsonify(success=False, message="Missing JSON body", data=None)
+
+    success, token = database_helper.sign_in(data.get("email"), data.get("password"))
+
+    if not success:
+        return jsonify(success=False, message="Invalid username or password"), 400
+    
+    return jsonify(success=True, message="Successfully logged in", data=token), 200
